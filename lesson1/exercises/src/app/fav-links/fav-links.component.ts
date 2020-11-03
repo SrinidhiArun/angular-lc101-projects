be88f753dbf2514845b1759c7639c3b6e05879fd
Arun Balasubramanian <srinidhi>
2020-11-03 15:35:07 -0600

    Angular part 1 exercise

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ["https://www.w3schools.com/","https://github.com/SrinidhiArun"];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://springsrealtyaz.com/wp-content/uploads/2018/05/city-dark-dawn-248159-1024x1024.jpg';
   image3 = '';
 
   constructor() { }
